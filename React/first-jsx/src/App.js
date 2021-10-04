a61d32e8531bbe8a5032cb1ccac07d13052c9fd4
Austin Nye <amnye0815@gmail.com>
2021-10-04 14:42:40 -0700

    final commits

--- React/first-jsx/src/App.js
+++ React/first-jsx/src/App.js
@@ -8,8 +8,8 @@ function App() {
       <h3>Things I need to do:</h3>
       <ul>
         <li>Learn React</li>
-        <li>Climb Mt. Everest</li>
-        <li>Run a maraton</li>
+        <li>Summit Mt. Rainier</li>
+        <li>Clean my apartment</li>
         <li>Feed the dogs</li>
       </ul>
     </div>
