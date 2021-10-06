fc304c136998e662e939e4219e6f1b414d9bc9bf
Austin Nye <amnye0815@gmail.com>
2021-10-05 20:28:07 -0700

    final commits

--- React/putting-it-together/src/components/PersonCard.js
+++ React/putting-it-together/src/components/PersonCard.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 
 const PersonCard = (props) => {
-    const { name, haircolor } = props;
-    const [age, setAge] = useState();
+    const { name, age, haircolor } = props;
+    const [bdayAge, setBdayAge] = useState(age);
 
     const handleClick = (e) => {
 
-        setAge(age+1);
+        setBdayAge(bdayAge+1);
         console.log(e);
         console.log(e.target);
 
@@ -15,7 +15,7 @@ const PersonCard = (props) => {
     return (
         <div>
             <h1>{ name }</h1>
-            <p>Age: { props.age }</p>
+            <p>Age: { bdayAge }</p>
             <p>Hair Color: { haircolor }</p>
             <button onClick={ handleClick }>Birthday Button for { name }</button>
         </div>
