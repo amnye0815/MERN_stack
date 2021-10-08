3bf955cdb7bee56f71aece99aac9acb3947b3bb8
Austin Nye <amnye0815@gmail.com>
2021-10-08 10:29:11 -0700

    initial commits

---
+++ React/hook-form/src/components/UserForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+const UserForm = (props) => {
+    const [first, setFirst] = useState("");
+    const [last, setLast] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirm, setConfirm] = useState("");
+
+    const createUser = (e) => {
+        e.preventDefault();
+
+        const newUser = { first, last, email, password, confirm };
+        setFirst("");
+        setLast("");
+        setEmail("");
+        setPassword("");
+        setConfirm("");
+        };
+        console.log("Welcome", newUser);
+    };
+    return (
+        <form onSubmit={ createUser }>
+            <div>
+                <label>First Name:</label>
+                <input type="text" value={ first } onChange={ (e) => setFirst(e.target.value) } />
+            </div>
+            <div>
+                <label>Last Name:</label>
+                <input type="text" value={ last } onChange={ (e) => setLast(e.target.value) } />
+            </div>
+            <div>
+                <label>Email:</label>
+                <input type="text" value={ email } onChange={ (e) => setEmail(e.target.value) } />
+            </div>
+            <div>
+                <label>Password:</label>
+                <input type="password" value={ password } onChange={ (e) => setPassword(e.target.value) } />
+            </div>
+            <div>
+                <label>Confirm Password:</label>
+                <input type="password" value={ confirm } onChange={ (e) => setConfirm(e.target.value) } />
+            </div>
+            <input type="submit" value="Create User" />
+        </form>
+    );
+};
+export default UserForm;
