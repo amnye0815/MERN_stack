6913f1c726a366617d94cf2aa173073517375f73
Austin Nye <amnye0815@gmail.com>
2021-10-12 11:24:12 -0700

    set-up complete

---
+++ React/todo-list/src/components/Display.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Display = () => {
+
+    return (
+        <div>
+            <h3><u>To Do List</u></h3>
+            <div>
+                <label>iterate items with map here</label>
+                <input type="checkbox" />
+                <button>Delete</button>
+            </div>
+        </div>
+    )
+}
+
+export default Display;
---
+++ React/todo-list/src/components/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Form = () => {
+
+    return(
+        <form>
+            <h3><u>Add a Task</u></h3>
+            <input type="text" />
+            <input type="submit" value="Add" />
+        </form>
+    )
+}
+
+export default Form;
