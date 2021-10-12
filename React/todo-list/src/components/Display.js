02cf124b76dd49f8adcec1b170500c7402165a9e
Austin Nye <amnye0815@gmail.com>
2021-10-12 11:47:14 -0700

    Set up complete

--- React/todo-list/src/components/Display.js
+++ React/todo-list/src/components/Display.js
@@ -3,17 +3,21 @@ import React from 'react';
 const Display = (props) => {
     const { newTask } = props;
 
+    // ADD DELETE FUNCTION OF ONCLICK HANDLER
+    // ADD STRIKETHROUGH CSS WITH CHECKED? TERNARY OPERATOR
     return (
         <div>
             <h3><u>To Do List</u></h3>
             <div>
                 {
                 newTask.map((task, index)=>(
-                        <label key={index}>{task.content}</label>
+                    <div key={index}>
+                        <label>{task.content}</label>
+                        <input type="checkbox" />
+                        <button>Delete</button>
+                    </div>
                     ))
                 }
-                <input type="checkbox" />
-                <button>Delete</button>
             </div>
         </div>
     )
