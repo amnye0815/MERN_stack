63398e0d9fcab85417b53ccd7cc6832a38325946
Austin Nye <amnye0815@gmail.com>
2021-10-12 10:49:27 -0700

    hmmmm....

--- React/tabs/src/components/TabContentDisplay.js
+++ React/tabs/src/components/TabContentDisplay.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const TabContentDisplay = (props) => {
-    const { tabContent, setTabContent } = props;
+    const tabContent = ["This is the yellow tab. It should change the window to yellow.", 
+    "This is the magenta tab. It should change the window to magenta.", 
+    "This is the cyan tab. It should change the window to cyan.",
+    ];
 
     return (
         <p style={{ alignItems: "center", justifyContent: "center", height: "150px", marginLeft: "650px", marginRight: "650px", border: "3px solid black" }}>This is the tab content</p>
