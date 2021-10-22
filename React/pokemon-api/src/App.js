375c1c6dbbf539a02ba6c92d63b627f7a53c3e02
Austin Nye <amnye0815@gmail.com>
2021-10-22 13:20:04 -0700

    initial and final commits

--- React/pokemon-api/src/App.js
+++ React/pokemon-api/src/App.js
@@ -5,28 +5,19 @@ import React, { useEffect, useState } from 'react';
 function App() {
   const [pokemon, setPokemon] = useState([]);
 
-  useEffect(()=>{
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=808')
-      .then((res)=>{
-        return res.json();
-      })
-      .then((res)=>{
-        setPokemon(res.results);
-      })
-      .catch((err)=>{
-        console.log(err);
-      })
+  useEffect(() =>{
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+    .then (response => response.json())
+    .then (response => setPokemon(response.results))
   }, []);
-  
+
   return (
     <div className="App">
-      <h3>Pokemon - <span className="practice">All 807 of them!</span></h3>
+      <button>Fetch Pokemon</button>
       <ul>
-        {
-          pokemon.map((pokemonByName, index)=>{
-            return (<li key={index}>{pokemonByName.name}</li>)
-          })
-        }
+        {pokemon.length > 0 && pokemon.map((pokemon, index) =>{
+          return(<li key={index}>{pokemon.name}</li>)
+        })}
       </ul>
     </div>
   );
