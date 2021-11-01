1924664f0e91afc38ad53b2f17918a3804cc298a
Austin Nye <amnye0815@gmail.com>
2021-11-01 11:04:57 -0700

    finished

--- Express/Faker_API/server.js
+++ Express/Faker_API/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const faker = require("faker");
 const app = express();
 
+app.use(express.json() );
+app.use(express.urlencoded({extended: true}));
+
 app.get("/api/users/new", (req, res) => {
     res.json({newFakeUser});
 });
