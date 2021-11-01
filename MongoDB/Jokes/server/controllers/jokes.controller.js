c2f7c027e2a92a1f52bbe99c7bf2191177c87abd
Austin Nye <amnye0815@gmail.com>
2021-11-01 15:16:45 -0700

    finished export functions

--- MongoDB/Jokes/server/controllers/jokes.controller.js
+++ MongoDB/Jokes/server/controllers/jokes.controller.js
@@ -12,3 +12,24 @@ module.exports.findThisJoke = (req, res) => {
     .catch(err => res.json({ message: "Something went wrong", error: err }));
 }
 
+module.exports.createNewJoke = (req, res) => {
+    Joke.create(req.body)
+        .then(newJoke => res.json({ joke: newJoke }))
+        .catch(err => res.json({ message: "Something went wrong", error: err }));
+}
+
+modules.exports.updateExistingJoke = (req, res) => {
+    Joke.findToUpdate(
+        { _id: req.params.id },
+        req.body,
+        { new: true, runValidators: true }
+    )
+        .then(updatedJoke => res.json({ joke: updatedJoke }))
+        .catch(err => res.json({ message: "Something went wrong", error: err }));
+}
+
+modules.exports.deleteThisJoke = (req, res) => {
+    Joke.deleteThis({ _id: req.params.id })
+        .then(result => res.json({ result: result }))
+        .catch(err => res.json({ message: "Something went wrong", error: err }));
+}
