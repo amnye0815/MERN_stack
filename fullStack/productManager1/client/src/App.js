2fc4b92d817506daa8f17dd7e9961cc43e4708e6
Austin Nye <amnye0815@gmail.com>
2021-11-02 16:47:21 -0700

    middle of the road

--- fullStack/productManager1/client/src/App.js
+++ fullStack/productManager1/client/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Main from './views/Main';
-import Form from './components/Form';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Main />
-      <Form />
     </div>
   );
 }
--- fullStack/productManager1/client/src/views/Main.js
+++ fullStack/productManager1/client/src/views/Main.js
@@ -1,15 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Form from './components/Form';
 
 const Main = () => {
-    const [ product, setProduct ] = useState([]);
-    useEffect(()=> {
-        axios.get("http://localhost:8000")
-            .then(res=> setProduct(res.data.product))
-    }, []);
     return(
         <div>
             <h1>Product Manager</h1>
+            <Form />
         </div>
     )
 }
