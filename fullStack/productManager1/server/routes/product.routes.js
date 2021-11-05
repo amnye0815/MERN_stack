5a339939d5b53e8e54796f7fac903b5a3bd1ceef
Austin Nye <amnye0815@gmail.com>
2021-11-05 11:19:26 -0700

    staging for product manager 1 only

--- fullStack/productManager1/server/routes/product.routes.js
+++ fullStack/productManager1/server/routes/product.routes.js
@@ -1,9 +1,9 @@
 const ProductController = require("../controllers/product.controller");
 
 module.exports = app => {
-    app.get('/api/products', ProductController.findAllProducts);
+    // app.get('/api/products', ProductController.findAllProducts);
     app.post('/api/products', ProductController.createProduct);
-    app.get('/api/products/:id', ProductController.findThisProduct);
-    app.put('/api/products/:id', ProductController.updateExistingProduct);
-    app.delete('/api/products/:id', ProductController.deleteThisProduct);
+    // app.get('/api/products/:id', ProductController.findThisProduct);
+    // app.put('/api/products/:id', ProductController.updateExistingProduct);
+    // app.delete('/api/products/:id', ProductController.deleteThisProduct);
 }
