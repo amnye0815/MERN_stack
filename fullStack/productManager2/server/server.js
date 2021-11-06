caf9a25870e961ddc0c8c713e9857babce715b01
Austin Nye <amnye0815@gmail.com>
2021-11-06 11:28:52 -0700

    back end set up

--- fullStack/productManager2/server/server.js
+++ fullStack/productManager2/server/server.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-app.listen(8000, () => console.log("Listening on Port 8000."));
+// Middleware has to be above our imports.
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+require('./config/mongoose.config')
+require('./routes/product.routes')(app);
+
+
+app.listen(8000, () => console.log('Listening on Port 8000.'));
