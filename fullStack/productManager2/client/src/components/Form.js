20f910c6b831e30d4adc1947aa9ddbd31203ded9
Austin Nye <amnye0815@gmail.com>
2021-11-06 11:30:41 -0700

    front end set up

---
+++ fullStack/productManager2/client/src/components/Form.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Form = (props) => {
+    // const {product, setProduct} = props;
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState("");
+    const [desc, setDesc] = useState("");
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        axios.post("http://localhost:8000/api/products", {
+            title: title,
+            price: price,
+            desc: desc
+        })
+            .then(res=> console.log(res))
+            .catch(err=> console.log(err))
+
+        // setProduct([...product, {title: title, price: price, desc: desc}]);
+        setTitle("");
+        setPrice("");
+        setDesc("");
+    }
+
+    return (
+        <form onSubmit={ submitHandler }>
+            <div>
+                <label>Title: </label>
+                <input type="text" value={ title } onChange={(e)=> setTitle(e.target.value)} />
+            </div>
+            <div>
+                <label>Price: </label>
+                <input type="text" value={ price } onChange={(e)=> setPrice(e.target.value)} />
+            </div>
+            <div>
+                <label>Description: </label>
+                <input type="text" value={ desc } onChange={(e)=> setDesc(e.target.value)} />
+            </div>
+            <input type="submit" />
+        </form>
+    )
+}
+
+export default Form;
---
+++ fullStack/productManager2/client/src/views/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from '../components/Form';
+
+const Main = () => {
+    return(
+        <div>
+            <h1>Product Manager</h1>
+            <Form />
+        </div>
+    )
+}
+export default Main;
