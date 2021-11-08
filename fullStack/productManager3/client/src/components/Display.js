0d7f2833e6384a9276d79c1040299de06df12f61
Austin Nye <amnye0815@gmail.com>
2021-11-08 11:40:19 -0800

    set up complete

---
+++ fullStack/productManager3/client/src/components/Display.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+import { Link } from '@reach/router';
+
+const Display = (props) => {
+    return (
+        <div>
+            <h1>Current Products</h1>
+            {props.product.map((product, index) =>{
+                return <div>
+                    <Link to = "/products/:id" key={index}>{product.title}</Link>
+                    </div>
+            })}
+        </div>
+    )
+}
+export default Display;
---
+++ fullStack/productManager3/client/src/views/Main.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Display from '../components/Display';
+import Form from '../components/Form';
+
+const Main = () => {
+    const [product, setProduct] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/products')
+            .then(res=> {
+                setProduct(res.data);
+                setLoaded(true);
+            });
+    }, [])
+
+    return(
+        <div>
+            <Form />
+            <hr/>
+            { loaded && <Display product={product}/> }
+        </div>
+    )
+}
+export default Main;
