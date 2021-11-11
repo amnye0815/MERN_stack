021d942a39110432b252b5ce4a4245e58179877e
Austin Nye <amnye0815@gmail.com>
2021-11-10 21:06:31 -0800

    Model updated

---
+++ Express/Faker_API/server-DESKTOP-6233746.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const faker = require("faker");
+const app = express();
+
+app.use(express.json() );
+app.use(express.urlencoded({extended: true}));
+
+app.get("/api/users/new", (req, res) => {
+    res.json({newFakeUser});
+});
+
+app.get("/api/companies/new", (req, res)=> {
+    res.json({newFakeCompany});
+});
+
+app.get("/api/user/company", (req, res) => {
+    res.json({newFakeUser});
+    res.json({newFakeCompany});
+});
+
+const createUser = () => {
+    const newFakeUser =  {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        phoneNumber: faker.phone.phoneNumber(),
+        email: faker.internet.email(),
+        password: faker.internet.password()
+    };
+    return newFakeUser
+};
+
+const newFakeUser = createUser();
+console.log(newFakeUser);
+
+const createCompany = () => {
+    const newFakeCompany = {
+        name: faker.company.companyName(),
+        street: faker.address.streetAddress(),
+        city: faker.address.cityName(),
+        state: faker.address.state(),
+        zipCode: faker.address.zipCode(),
+        country: faker.address.country()
+    };
+    return newFakeCompany;
+};
+
+const newFakeCompany = createCompany();
+console.log(newFakeCompany);
--- Express/Faker_API/server.js
+++ Express/Faker_API/server.js
@@ -1,48 +1,56 @@
 const express = require("express");
-const faker = require("faker");
+const faker = require('faker');
 const app = express();
+const port = 8000;
 
-app.use(express.json() );
-app.use(express.urlencoded({extended: true}));
+app.listen( port, () => console.log(`Listening on port: ${port}`) );
 
-app.get("/api/users/new", (req, res) => {
-    res.json({newFakeUser});
+app.get("/api", (req, res) => {
+    res.json("Faker API");
 });
 
-app.get("/api/companies/new", (req, res)=> {
-    res.json({newFakeCompany});
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
+
+const createUser = () => {
+    return {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        phoneNumber: faker.phone.phoneNumberFormat(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+    };
+};
+
+const createCompany = () => {
+    return {
+        name: faker.company.companyName(),
+        streetAddress: faker.address.streetAddress(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        zipCode: faker.address.zipCode(),
+        country: faker.address.country(),
+    };
+};
+
+app.get("/api/users/new", (req, res) => {
+    const newUser = createUser();
+    console.log(newUser);
+    res.json(newUser);
+});
+
+app.get("/api/companies/new", (req, res) => {
+    const newCompany = createCompany();
+    console.log(newCompany);
+    res.json(newCompany);
 });
 
 app.get("/api/user/company", (req, res) => {
-    res.json({newFakeUser});
-    res.json({newFakeCompany});
+    let newUser = createUser();
+    let newCompany = createCompany();
+
+    console.log (newUser);
+    console.log (newCompany);
+
+    res.json({ user: newUser, company: newCompany });
 });
-
-const createUser = () => {
-    const newFakeUser =  {
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-        phoneNumber: faker.phone.phoneNumber(),
-        email: faker.internet.email(),
-        password: faker.internet.password()
-    };
-    return newFakeUser
-};
-
-const newFakeUser = createUser();
-console.log(newFakeUser);
-
-const createCompany = () => {
-    const newFakeCompany = {
-        name: faker.company.companyName(),
-        street: faker.address.streetAddress(),
-        city: faker.address.cityName(),
-        state: faker.address.state(),
-        zipCode: faker.address.zipCode(),
-        country: faker.address.country()
-    };
-    return newFakeCompany;
-};
-
-const newFakeCompany = createCompany();
-console.log(newFakeCompany);
