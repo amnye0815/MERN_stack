e1e3bc1a00f8d75a4b7be2114210e99e99af6970
Austin Nye <amnye0815@gmail.com>
2021-11-10 21:45:52 -0800

    app.js complete

--- fullStack/authors/client/src/App.js
+++ fullStack/authors/client/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import {Router} from '@reach/router';
+import Form from './components/Form';
+import Display from './components/Display';
+import Update from './components/Update';
+
 
 function App() {
   return (
     <div className="App">
       <h1><u>Favorite Authors</u></h1>
-      
+      <Router>
+        <Display path="/" />
+        <Form path="/new" />
+        <Update path="/edit/:id" />
+      </Router>
     </div>
   );
 }
