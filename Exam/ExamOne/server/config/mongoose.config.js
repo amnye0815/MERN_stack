1dbffb1e71760384b83df64932805ca35e19275a
Austin Nye <79955532+amnye0815@users.noreply.github.com>
2021-11-12 13:38:06 -0800

    Delete Exam/ExamOne directory
    Need to put in private repo

--- Exam/ExamOne/server/config/mongoose.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const dbName = 'pets';
-
-mongoose.connect(`mongodb://localhost/${dbName}`, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-})
-    .then(()=> console.log(`Established connection to ${dbName} database!`))
-    .catch(err => console.log(err))
